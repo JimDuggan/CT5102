0dd3499c4e8e47f9eed2e453b23568cfbc2f0527
jimduggan <jim.duggan@nuigalway.ie>
2018-09-03 09:07:38 +0100

    Challenge 1.2 example

--- code/01 Vectors/Challenge 1.2.R
+++ code/01 Vectors/Challenge 1.2.R
@@ -4,6 +4,17 @@
 #   Find the average
 #   Subset all those values greater than the average
 
+v <- (1:10)^2
+
+mn <- min(v)
+
+mx <- max(v)
+
+f <- v > mean(v)
+
+v[f]
+
+f1 <- v[v > mean(v)]
 
 
 
