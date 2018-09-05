0b4799f9952e29c145fc15b5e21e7ff4f87a4478
jimduggan <jim.duggan@nuigalway.ie>
2018-09-05 20:27:25 +0100

    Updating code examples

---
+++ code/02 Lists and Functions/function.R
@@ -0,0 +1,13 @@
+# An example of a function in R
+# Add two numbers together
+# Inputs (a,b) - two numbers
+# Output - sum of two
+
+add <- function(a,b){
+  a+b
+}
+
+
+
+
+
---
+++ code/02 Lists and Functions/list.R
@@ -0,0 +1,9 @@
+# create a list an manipulate the elements
+# list contains three elements
+# an integer vector, a string and a boolean vector
+
+l <- list(1:2,"Test",c(T,F))
+
+
+
+
